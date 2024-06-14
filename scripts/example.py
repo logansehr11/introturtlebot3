92ed48487048a433895d3b7dd2be9b8e3a431b0d
logansehr11 <logansehr@gmail.com>
2024-06-14 13:58:27 -0400

    Updated Map and moved Code to Scripts

--- scripts/example.py
+++ scripts/example.py
@@ -13,11 +13,11 @@ class Waypoint(Node):
         super().__init__('nav2_waypoint')
         self.timer = self.create_timer(1, self.timer_callback)
         self.navigator = BasicNavigator()
-        self.hri_route = [[2.587, -3.507],
-                        [5.324, -5.464],
-                        [6.298, -1.936],
-                        [1.720, 1.430],
-                        [-0.473, -1.339]]
+        self.hri_route = [[6.049, 1.678],
+                        [-0.233, 0.077],
+                        [-0.394, -3.464],
+                        [2.867, -3.499],
+                        [6.592, -3.970]]
         self.total = 5
         self.time = 0
     
@@ -26,8 +26,8 @@ class Waypoint(Node):
         pose = PoseStamped()
         pose.header.frame_id = 'map'
         pose.header.stamp = self.navigator.get_clock().now().to_msg()
-        pose.pose.position.x = 2.587
-        pose.pose.position.y = -3.507
+        pose.pose.position.x = 6.049
+        pose.pose.position.y = 1.678
         pose.pose.orientation.z = 1.0
         pose.pose.orientation.w = 0.0
         self.navigator.setInitialPose(pose)
@@ -54,4 +54,4 @@ def main(args=None):
 if __name__ == '__main__':
     main()
 
-# Updated Points: 2.587 -3.507 -- 5.324 -5.464 -- 6.298 -1.936 -- 1.720 1.430 -- -0.473 -1.339
+# Updated Points: 6.049 1.678 -- -0.233 0.077 -- -0.394 -3.464 -- 2.867 -3.499 -- 6.592 -3.970
