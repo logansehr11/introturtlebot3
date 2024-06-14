0146ba816fc34854f0a07356789d0bad69c705ce
logansehr11 <logansehr@gmail.com>
2024-06-14 15:17:44 -0400

    Created Script Launch File and Updated Code

---
+++ launch/waypoint.launch.py
@@ -0,0 +1,15 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    return LaunchDescription([
+        Node(
+            package='turtlebot3_navigation',
+            executable='example_node',
+            name='example_node',
+            output='screen',
+        ),
+    ])
--- scripts/example.py
+++ scripts/example.py
@@ -1,6 +1,7 @@
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
 from sensor_msgs.msg import LaserScan
 from rclpy.qos import ReliabilityPolicy, QoSProfile
@@ -11,6 +12,11 @@ class Waypoint(Node):
 
     def __init__(self):
         super().__init__('nav2_waypoint')
+
+        self.publisher = self.create_publisher(Twist, 'cmd_vel', 10)
+        self.pub_timer = self.create_timer(1, self.spinner_callback)
+        self.seconds = 0
+
         self.timer = self.create_timer(1, self.timer_callback)
         self.navigator = BasicNavigator()
         self.hri_route = [[6.049, 1.678],
@@ -20,24 +26,31 @@ class Waypoint(Node):
                         [6.592, -3.970]]
         self.total = 5
         self.time = 0
-    
+
+
+    def spinner_callback(self):
+        message = Twist()
+        message.angular.z = 4.5
+        if (self.seconds < 4):
+            self.seconds += 1
+            self.publisher.publish(message)
+        else:
+            message.angular.z = 0.0
+            self.publisher.publish(message)
+            self.pub_timer.cancel()
+        
     def timer_callback(self):
         self.navigator.waitUntilNav2Active()
         pose = PoseStamped()
         pose.header.frame_id = 'map'
         pose.header.stamp = self.navigator.get_clock().now().to_msg()
-        pose.pose.position.x = 6.049
-        pose.pose.position.y = 1.678
-        pose.pose.orientation.z = 1.0
-        pose.pose.orientation.w = 0.0
-        self.navigator.setInitialPose(pose)
 
         for (posx, posy) in self.hri_route:
-            self.navigator.goToPose(pose)
-            self.time += 1
             pose.pose.position.x = posx
             pose.pose.position.y = posy
+            self.navigator.goToPose(pose)
             while not self.navigator.isTaskComplete():
+                self.time += 1
                 pass  
         feedback = self.navigator.getFeedback()
         self.timer.cancel()
